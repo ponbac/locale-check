765df3e45dac0eaefae09f217f4bdec30e5e74e6
ponbac <root@ponbac.xyz>
2023-08-18 22:39:42 +0200

    some npm stuff

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -1,9 +1,23 @@
 const { Binary } = require("binary-install");
+const os = require("os");
+
+function getPlatform() {
+  const type = os.type();
+  const arch = os.arch();
+
+  if (type === "Windows_NT" && arch === "x64") return "win64";
+  if (type === "Windows_NT") return "win32";
+  if (type === "Linux" && arch === "x64") return "linux";
+  if (type === "Darwin" && arch === "x64") return "macos";
+
+  throw new Error(`Unsupported platform: ${type} ${arch}`);
+}
 
 function getBinary() {
+  const platform = getPlatform();
   const version = require("../package.json").version;
-  const url = `https://github.com/username/my-program/releases/download/v${version}/my-program-win64.tar.gz`;
-  const name = "my-program";
+  const name = "check-translations";
+  const url = `https://github.com/ponbac/locale-check/releases/download/v${version}/${name}-${platform}.tar.gz`;
   return new Binary(url, { name });
 }
 
